6d5258389d126ea03563b6a63a8acad42a3a47fb
Totenem <yganamichaelashley@gmail.com>
2025-04-21 20:20:22 +0800

    lost of fix

--- test.py
+++
@@ -1,9 +0,0 @@
-from config import GROQ_API_KEY, SUPABASE_URL, SUPABASE_KEY
-from supabase import create_client, Client
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-user_id = "b1334885-5510-4897-a0fa-a38df2cc7e96"
-
-# Fetch user profile
-response = supabase.table("user_profiles").select("*").eq("id", user_id).execute()
-print(response)
